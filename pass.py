23c4301879c49371ad667c7075d8d8ff228cd17c
Shreyansh Chaturvedi <133202898+chaturvedi888@users.noreply.github.com>
2024-04-10 15:58:00 +0530

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,27 +1,17 @@
 import random
 import string
-
 def generate_password(length=12):
-    # Define character sets
     uppercase_letters = string.ascii_uppercase
     lowercase_letters = string.ascii_lowercase
     digits = string.digits
     special_characters = string.punctuation
-
-    # Combine character sets
     all_characters = uppercase_letters + lowercase_letters + digits + special_characters
-
-    # Ensure at least one character from each set is included
     password = random.choice(uppercase_letters)
     password += random.choice(lowercase_letters)
     password += random.choice(digits)
     password += random.choice(special_characters)
-
-    # Fill the rest of the password with random characters
     for _ in range(length - 4):
         password += random.choice(all_characters)
-
-    # Shuffle the password characters
     password_list = list(password)
     random.shuffle(password_list)
     password = ''.join(password_list)
